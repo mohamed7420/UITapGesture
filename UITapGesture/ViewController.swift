3a53de57bd5302ea2782122d22cec9ad1713b01d
mohamedosama2074 <iosengks74@gmail.com>
2019-08-21 00:32:51 +0200

    UISingleTapGesture

--- UITapGesture/ViewController.swift
+++ UITapGesture/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageMohamed: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        // one tap on image
+        
+        
+        imageMohamed.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleImageTapGesture(recognizer:)))
+        tapGesture.numberOfTapsRequired = 1
+        imageMohamed.addGestureRecognizer(tapGesture)
+    }
+    
+    
+    @objc func handleImageTapGesture(recognizer : UITapGestureRecognizer){
+        
+        if imageMohamed.image == UIImage(named: "mohamed"){
+            
+            imageMohamed.image = UIImage(named: "screen")
+            
+        }else if imageMohamed.image == UIImage(named: "screen") {
+            
+            imageMohamed.image = UIImage(named: "mohamed")
+        }
+        
     }
 
 
